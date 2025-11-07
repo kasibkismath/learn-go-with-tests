419aeeb14ae8a73cca7208b29e8f9bed692f3069
kasibkismath@gmail.com <kasibkismath@gmail.com>
2025-11-07 22:37:22 +0530

    feat: hello world with test cases

---
+++ hello/hello_test.go
@@ -0,0 +1,23 @@
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Kasib")
+		want := "Hello, Kasib!"
+		assert(t, got, want)
+	})
+	t.Run("say hello world when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World!"
+		assert(t, got, want)
+	})
+}
+
+func assert(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
