a66030c96d8e9d101e9fc101319095b8b66f7ebc
kasibkismath@gmail.com <kasibkismath@gmail.com>
2025-11-16 18:20:17 +0530

    feat: struct, types and interfaces basics

---
+++ structs-methods-interfaces/shapes_test.go
@@ -0,0 +1,34 @@
+package shapes
+
+import (
+	"testing"
+)
+
+func TestPerimeter(t *testing.T) {
+	rectangle := Rectangle{10.0, 10.0}
+	got := Perimeter(rectangle)
+	want := 40.0
+	if got != want {
+		t.Errorf("got %.2f want %.2f", got, want)
+	}
+}
+
+func TestArea(t *testing.T) {
+
+	checkArea := func(t testing.TB, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
+	}
+	t.Run("rectangle", func(t *testing.T) {
+		rectangle := Rectangle{10.0, 10.0}
+		checkArea(t, rectangle, 100.0)
+	})
+
+	t.Run("circle", func(t *testing.T) {
+		cirlce := Circle{10.0}
+		checkArea(t, cirlce, 314.1592653589793)
+	})
+}
