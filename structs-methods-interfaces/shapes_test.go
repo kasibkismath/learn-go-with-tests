ca3c1b079118c4d8c13f03b57f6cab80c789010b
kasibkismath@gmail.com <kasibkismath@gmail.com>
2025-11-16 19:48:24 +0530

    refactor: structs, methods and interfaces

--- structs-methods-interfaces/shapes_test.go
+++ structs-methods-interfaces/shapes_test.go
@@ -32,3 +32,23 @@ func TestArea(t *testing.T) {
 		checkArea(t, cirlce, 314.1592653589793)
 	})
 }
+
+func TestAreaNew(t *testing.T) {
+	areaTests := []struct {
+		name    string
+		shape   Shape
+		hasArea float64
+	}{
+		{name: "Rectangle", shape: Rectangle{10, 10}, hasArea: 100.0},
+		{name: "Circle", shape: Circle{10}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{12, 6}, hasArea: 36.0},
+	}
+	for _, tt := range areaTests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.hasArea {
+				t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasArea)
+			}
+		})
+	}
+}
